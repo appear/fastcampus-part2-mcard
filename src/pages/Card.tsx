12972dd21522554e38d5f49e9e59a81e37c08342
appear <appear.ko@gmail.com>
2023-09-16 16:21:32 +0900

    feat: 병목코드 개선

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -124,22 +124,7 @@ function IconCheck() {
 }
 
 function removeHtmlTags(text: string) {
-  let output = ''
-
-  for (let i = 0; i < text.length; i += 1) {
-    if (text[i] === '<') {
-      for (let j = i + 1; j < text.length; j += 1) {
-        if (text[j] === '>') {
-          i = j
-          break
-        }
-      }
-    } else {
-      output += text[i]
-    }
-  }
-
-  return output
+  return text.replace(/<\/?[^>]+(>|$)/g, '')
 }
 
 const termsContainerStyles = css`
