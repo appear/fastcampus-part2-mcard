64c7262b04ea5a2c97f7b5a0b2a865a3aa1bda5d
appear <appear.ko@gmail.com>
2023-09-16 20:11:13 +0900

    chore: 불필요한 코드제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { Suspense } from 'react'
 import Top from '@shared/Top'
 import AdBanners from '@components/home/AdBanners'
 import CardList from '@components/home/CardList'
-import Button from '@shared/Button'
 import ListRow from '@shared/ListRow'
 
 function HomePage() {
@@ -13,9 +12,7 @@ function HomePage() {
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
       />
-      <Button>안녕하세요</Button>
       <AdBanners />
-      <Button>안녕하세요</Button>
       <Suspense
         fallback={[...new Array(10)].map((_, idx) => (
           <ListRow.Skeleton key={idx} />
@@ -23,7 +20,6 @@ function HomePage() {
       >
         <CardList />
       </Suspense>
-      <Button>안녕하세요</Button>
     </div>
   )
 }
